acd991c57575722e1b52808c99e02fb23f3cba07
ddongseop <dlehdtjq00o@gmail.com>
2023-05-16 22:16:19 +0900

    [Chore] 프로젝트 초기 세팅 완료

---
+++ src/main/java/sopt/org/Pinterest/PinterestApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.Pinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/org/Pinterest/PinterestApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.Pinterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
