1edabe3be64df8d1d6108a637cd528cf23083b24
sunseo18 <sunseo1818@gmail.com>
2023-05-18 01:04:07 +0900

    [Feat] Entity 구현

---
+++ src/main/java/sopt/org/Pinterest/domain/Pin.java
@@ -0,0 +1,54 @@
+package sopt.org.Pinterest.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Pin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user; //게시글을 작성한 사용자 정보
+
+    @Column
+    private String renderUrl;
+
+    @Column
+    private String altTxt;
+
+    @OneToMany(mappedBy = "pin")
+    private List<Comment> commentList = new ArrayList<>();
+
+    private Pin(String title, String content, String image, User user, String renderUrl, String altTxt) {
+        this.title = title;
+        this.content = content;
+        this.image = image;
+        this.user = user;
+        this.renderUrl = renderUrl;
+        this.altTxt = altTxt;
+    }
+
+    public static Pin newInstance(String title, String content, String image, User user, String renderUrl, String altTxt) {
+        return new Pin(title, content, image, user, renderUrl, altTxt);
+    }
+}
---
+++ src/main/java/sopt/org/Pinterest/domain/User.java
@@ -0,0 +1,57 @@
+package sopt.org.Pinterest.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String account;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer follower;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer following;
+
+    @Column
+    private String image;
+
+    @ManyToMany
+    @JoinTable(name = "scrap",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name="pin_id")
+    )
+    private List<Pin> savedPinList = new ArrayList<>();
+
+    private User(String account, String nickname, String image) {
+        this.account = account;
+        this.nickname = nickname;
+        this.image = image;
+
+        this.follower = 0;
+        this.following = 0;
+    }
+
+    public static User newInstance(String account, String nickname, String image) {
+        return new User(account, nickname, image);
+    }
+}
