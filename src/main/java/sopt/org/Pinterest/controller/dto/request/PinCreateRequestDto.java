2e7225ac1630b98075560ed9e052ae3af7714787
sunseo18 <sunseo1818@gmail.com>
2023-05-19 18:36:21 +0900

    [Fix] pin 생성하기에 title 빈 값 금지

--- src/main/java/sopt/org/Pinterest/controller/dto/request/PinCreateRequestDto.java
+++ src/main/java/sopt/org/Pinterest/controller/dto/request/PinCreateRequestDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PinCreateRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
 
     @NotBlank
